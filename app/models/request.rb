fac1a063029bda7697a911eb773ecc328e89014d
randallreedjr <randallreedjr@gmail.com>
2014-07-26 14:45:54 -0400

    Create associations in models

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,4 @@
 class Request < ActiveRecord::Base
+  has_many :request_theaters
+  has_many :theaters, through: :request_theaters
 end
--- app/models/theater.rb
+++ app/models/theater.rb
@@ -1,2 +1,6 @@
 class Theater < ActiveRecord::Base
+  has_many :theater_requests
+  has_many :requests, through: :theater_requests
+  has_many :showtimes
+  has_many :movies, through: :showtimes
 end
