fbaa351b87fc3d626354c90b4282750e2dab7dde
randallreedjr <randallreedjr@gmail.com>
2014-08-18 00:23:58 -0400

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140803004018) do
+ActiveRecord::Schema.define(version: 20140818042135) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20140803004018) do
     t.string   "formatted_address"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "zip_code"
   end
 
   create_table "showtimes", force: true do |t|
